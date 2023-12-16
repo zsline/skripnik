ff5d8181a8a4db1d823e00836b29613cecafef5d
zsline <sergei.dereka@gmail.com>
2023-12-16 14:10:50 +0200

    docs

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,9 @@
 const swiper1 = new Swiper('.testimonial__slider', {
-    // Optional parameters
     loop: true,
     slidesPerView: 4,
     spaceBetween: 40,
-    // Navigation arrows
     navigation: {
       nextEl: '.testimonial__next',
       prevEl: '.testimonial__prev',
     },
-  
   });
