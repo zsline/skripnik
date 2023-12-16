cd1a4a0962c35195503530911fb3a3818789705c
zsline <sergei.dereka@gmail.com>
2023-12-16 11:08:18 +0200

    partners

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+// const swiper1 = new Swiper('.testimonial__slider', {
+//     // Optional parameters
+//     loop: true,
+//     slidesPerView: 4,
+  
+//     // Navigation arrows
+//     navigation: {
+//     //   nextEl: '.swiper-button-next',
+//     //   prevEl: '.swiper-button-prev',
+//     },
+  
+//   });
+
+$(document).ready(function(){
+    $('.testimonial__slider').owlCarousel();
+  });
