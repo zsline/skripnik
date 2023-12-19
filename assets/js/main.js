7848b91fa956410f7fb5ae9a2a27d0c0d113b876
zsline <sergei.dereka@gmail.com>
2023-12-19 17:51:29 +0200

    header

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,19 @@ const swiper1 = new Swiper('.testimonial__slider', {
       prevEl: '.testimonial__prev',
     },
   });
+
+  const menu = document.querySelector('.header__menu');
+  const open = document.querySelector('.menu');
+  const close = document.querySelector('.close-menu');
+  const body = document.querySelector('body');
+
+  open.addEventListener('click', function () {
+    menu.classList.add('open-menu');
+    close.classList.add('close-menu__open');
+    body.style="overflow: hidden";
+  });
+  close.addEventListener('click', function () {
+    menu.classList.remove('open-menu');
+    close.classList.remove('close-menu__open');
+    body.style="overflow: auto";
+  });
